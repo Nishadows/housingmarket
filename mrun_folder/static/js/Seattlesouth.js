8e4aacec1a0768530aed8c1d406e462e1de6926c
Mrunmayee Kulkarni <kulkarnimrinmayee@gmail.com>
2019-08-22 09:51:06 -0400

    pushing all the commits-without the API key to the blank html

--- mrun_folder/static/js/Seattlesouth.js
+++ mrun_folder/static/js/Seattlesouth.js
@@ -1,5 +1,8 @@
 var apiKey="";
 
+// var APIKey=config.apikey;
+// import apiConfig from 'config';
+
 /** function to select data
 * @param {array} rows
 * @param {integer} index
@@ -13,11 +16,6 @@ function unpack(rows,index){
 function getYearlyData(){
     var queryURL=`https://www.quandl.com/api/v3/datasets/NAHB/HOI_AREA_TACOMALAKEWOODWA.json?start_date=1991-03-31&end_date=2019-03-31&collapse=yearly&api_key=${apiKey}`;
     d3.json(queryURL).then(function(data){
-        var quarter=unpack(data.dataset.data,0);
-        var medianprice=unpack(data.dataset.data,1);
-        var housing_op=unpack(data.dataset.data,2);
-        var medianincome=unpack(data.dataset.data,3);
-        var nationalrank=unpack(data.dataset.data,4);
         
     });
 }
@@ -25,15 +23,12 @@ function getYearlyData(){
 function buildPlot(){
     var url=`https://www.quandl.com/api/v3/datasets/NAHB/HOI_AREA_TACOMALAKEWOODWA.json?start_date=1991-03-31&end_date=2019-03-31&api_key=${apiKey}`;
     d3.json(url).then(function(data){
-        var name=data.dataset.name;
-        var metroarea=data.dataset.dataset_code;
         var startDate=data.dataset.start_date;
         var endDate=data.dataset.end_state;
         var quarter=unpack(data.dataset.data,0);
         var medianprice=unpack(data.dataset.data,1);
         var housing_op=unpack(data.dataset.data,2);
         var medianincome=unpack(data.dataset.data,3);
-        var nationalrank=unpack(data.dataset.data,4);
 
         getYearlyData();
 
